37b2a8be5f9e0ee85ce455c10aaa2c8d0def5bd8
David Nielsen <david@graveyard.dk>
2018-11-03 13:07:40 +0100

    Version bump [skip ci]

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-
-
+ENV=$1
+TAG="qa-v$(cat VERSION)"
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 echo $SCRIPTPATH
 docker run --rm -v $SCRIPTPATH:/app treeder/bump patch
+git add . && git commit -m "Version bump [skip ci]"
+git tag $TAG
+
+git push origin master
+git push origin $TAG
