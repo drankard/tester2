14643b67767bb12b083308cb54d8402195852ff0
David Nielsen <david@graveyard.dk>
2018-11-03 13:20:36 +0100

    Version 0.2.14 promoted to qa [skip ci]

--- promote.sh
+++ promote.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 ENV=$1
-TAG="qa-v$(cat VERSION)"
+VERSION="$(cat VERSION)"
+TAG="$ENV-v$VERSION"
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 echo $SCRIPTPATH
 docker run --rm -v $SCRIPTPATH:/app treeder/bump patch
-git add . && git commit -m "Version bump [skip ci]"
-git tag $TAG
+git add . && git commit -m "Version $VERSION promoted to $ENV [skip ci]"
+git tag $TAskip ciG
 
 git push origin master
 git push origin $TAG
