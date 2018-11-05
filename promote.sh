e84735913a33218bfed1ab34d034205cbb1b2741
David Nielsen <david@graveyard.dk>
2018-11-05 09:18:36 +0100

    tester

--- promote.sh
+++ promote.sh
@@ -12,5 +12,6 @@ echo "Updating git"
 git add . && git commit -m "Version $VERSION promoted to $ENV [skip ci]"
 git tag $TAG
 
-git push origin master
-git push origin $TAG
+git tag -l 
+#git push origin master
+#git push origin $TAG
