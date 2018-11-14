1e2e2d4e53d80c37dd2ffe243f298a1aa87b2f64
David Nielsen <david@graveyard.dk>
2018-11-14 11:42:17 +0100

    tester

--- promote.sh
+++ promote.sh
@@ -4,9 +4,5 @@ ENV=$1
 VERSION="$(cat VERSION)"
 TAG="${ENV}-v${VERSION}"
 
-docker run --rm -v ${PWD}:/app treeder/bump patch
-git add . && git commit -m "Version $VERSION $ENV [skip ci]"
-git push origin master
-
 git tag $TAG
 git push origin $TAG
