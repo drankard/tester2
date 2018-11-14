0cd24219f13d34f2be32d9c8a518d7f766b678d7
David Nielsen <david@graveyard.dk>
2018-11-14 11:23:36 +0100

    tester

--- promote.sh
+++ promote.sh
@@ -3,15 +3,10 @@
 ENV=$1
 VERSION="$(cat VERSION)"
 TAG="${ENV}-v${VERSION}"
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-echo $SCRIPTPATH
-echo "Executing docker"
-ls -l $SCRIPTPATH
+
 docker run --rm -v ${PWD}:/app treeder/bump patch
-echo "Updating git"
-
 git add . && git commit -m "Version $VERSION $ENV [skip ci]"
-git tag $TAG
-
 git push origin master
+
+git tag $TAG
 git push origin $TAG
